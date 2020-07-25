f4466ea22cfa9a49f688add473e840511644d7ae
Matti Mobolaji <mattimobolaji@gmail.com>
2020-07-25 16:42:06 +0100

    implement firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: 'AIzaSyApn3EKGheWhrxxYARZHWfEM4tvzqs8mhA',
+    authDomain: 'Partner-hero-assessment.firebaseapp.com',
+    databaseURL: 'https://partner-hero-assessment.firebaseio.com',
+    projectId: 'partner-hero-assessment',
+    storageBucket: 'partner-hero-assessment.appspot.com',
+    messagingSenderId: '427917447655',
+    appId: '1:427917447655:web:ab0de9cd1b6f4cfb40ed42',
+    measurementId: 'G-EZDGR1V7JC',
+};
